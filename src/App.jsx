4c63a5f325861a4d4d5e173b7a9ea43dfa5b34ab
ItachiPrabin <itachiprabin@gmail.com>
2025-01-19 22:50:44 +0545

    added service in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+// import Dashboard from "./core/public/pages/dashboard";
 import HomePage from "./core/public/pages/homepage";
 import LoginPage from "./core/public/pages/loginpage";
+import SignupPage from "./core/public/pages/signup";
+
+import ContactPage from "./core/public/pages/contactpage";
+import ServicePage from "./core/public/pages/servicepage";
 import "./index.css";
 
 const App = () => {
@@ -10,6 +15,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<SignupPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/service" element={<ServicePage />} />
+        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+
+
       </Routes>
     </Router>
   );
