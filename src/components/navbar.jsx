5fa95f2e8f4af258771c50e8182793c84e95ff11
ItachiPrabin <itachiprabin@gmail.com>
2025-01-21 22:16:09 +0545

    added profile on navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { FaDollarSign, FaHome, FaInfoCircle, FaPhoneAlt, FaServicestack } from "react-icons/fa"; // Import icons from react-icons
 
 const Navbar = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -9,86 +8,45 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-900">
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <img
-                    src="src\assets\images\cleanEase 1.png"
-                    className="h-8"
-                    alt="CleanEase"
-                />
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">CleanEase</span>
+        <nav className="bg-teal-600">
+            <div className="navbar bg-teal-400 text-white">
+                <div className="navbar-start">
+                    <a className="btn btn-ghost text-xl hover:bg-black">Clean Ease</a>
+                </div>
 
-                <button
-                    onClick={toggleMenu}
-                    type="button"
-                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-default"
-                    aria-expanded={isMenuOpen ? "true" : "false"}
-                >
-                    <span className="sr-only">Open main menu</span>
-                    <svg
-                        className="w-5 h-5"
-                        aria-hidden="true"
-                        xmlns="src\assets\images\cleanEase 1.png"
-                        fill="none"
-                        viewBox="0 0 17 14"
-                    >
-                        <path
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            d="M1 1h15M1 7h15M1 13h15"
-                        />
-                    </svg>
-                </button>
-                <div className={`w-full md:block md:w-auto ${isMenuOpen ? "block" : "hidden"}`} id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                                aria-current="page"
-                            >
-                                <FaHome className="w-5 h-5 inline-block" />
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                            >
-                                <FaInfoCircle className="w-5 h-5 inline-block" />
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                            >
-                                <FaServicestack className="w-5 h-5 inline-block" />
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                            >
-                                <FaDollarSign className="w-5 h-5 inline-block" />
-                            </a>
-                        </li>
-                        <li>
-                            <a
-                                href="#"
-                                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                            >
-                            </a>
-                            <button
-                                className="btn btn-primary" onClick={() => (window.location.href = "/contact")}>
-                                <FaPhoneAlt className="w-5 h-5 inline-block" />
-                            </button>
-                        </li>
+                <div className="navbar-center hidden lg:flex">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a className="hover:bg-black">Home</a></li>
+                        <li><a className="hover:bg-black">Service</a></li>
+                        <li><a className="hover:bg-black">Contact</a></li>
                     </ul>
+                </div>
+
+                <div className="navbar-end">
+                    <div className="dropdown dropdown-end">
+                        <div
+                            tabIndex={0}
+                            role="button"
+                            className="btn btn-ghost btn-circle avatar hover:bg-black"
+                        >
+                            <div className="w-10 rounded-full">
+                                <img alt="User Avatar" src="src/assets/images/iron.jpeg" />
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-teal-600 text-white rounded-box z-[1] mt-3 w-52 p-2 shadow"
+                        >
+                            <li>
+                                <a className="justify-between hover:bg-black">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a className="hover:bg-black">Settings</a></li>
+                            <li><a className="hover:bg-black">Logout</a></li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </nav>
@@ -96,4 +54,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
