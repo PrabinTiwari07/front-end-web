5e730db6cf0d7fcdc6f49ab0aa7d8b973906f648
ItachiPrabin <itachiprabin@gmail.com>
2025-02-02 21:58:49 +0545

    added routes for service page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-// import Dashboard from "./core/public/pages/dashboard";
+import Dashboard from "./core/public/pages/dashboard";
 import HomePage from "./core/public/pages/homepage";
 import LoginPage from "./core/public/pages/loginpage";
+import Signin from "./core/public/pages/signin";
 import SignupPage from "./core/public/pages/signup";
 
 import ContactPage from "./core/public/pages/contactpage";
 import ServicePage from "./core/public/pages/servicepage";
+
+import Sidebar from "./core/private/admin/pages/sidebar";
 import "./index.css";
 
 const App = () => {
@@ -15,12 +18,12 @@ const App = () => {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signin" element={<Signin />} />
         <Route path="/register" element={<SignupPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/service" element={<ServicePage />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
-
-
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sidebar" element={<Sidebar />} />
       </Routes>
     </Router>
   );
