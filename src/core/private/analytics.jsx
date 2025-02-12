fab8c234b92608b08c723e3ef386b9f2b75b79ee
ItachiPrabin <itachiprabin@gmail.com>
2025-02-12 23:47:33 +0545

    analytics page initialized

---
+++ src/core/private/analytics.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Analytics = () => {
+    return (
+        <div className="p-5 rounded-lg card shadow-md transition-all duration-300">
+            <h2 className="text-2xl font-bold mb-4">Analytics Overview</h2>
+            <p>Graph or charts go here...</p>
+        </div>
+    );
+};
+
+export default Analytics;
